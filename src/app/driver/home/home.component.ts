ee6a25952ae4744d43c4bc7868db74cb4a4e8087
Jaay7 <jayakrishna.madem@gmail.com>
2022-02-26 14:12:46 +0530

    retrieved accepted dealers in home

--- src/app/driver/home/home.component.ts
+++ src/app/driver/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DriverService } from 'src/app/services/driver.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  myDealers: any[] = [];
+
+  constructor(private driverService: DriverService) { }
 
   ngOnInit(): void {
+    this.driverService.getAcceptedDealers(Number(localStorage.getItem('userId'))).subscribe({
+      next: (response: any) => {
+        this.myDealers = response;
+        console.log(response);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+  
+  removeRequest(id: number, dealerId: number): void {
+    this.driverService.removeRequest(id, dealerId, Number(localStorage.getItem('userId'))).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
   }
 
 }
