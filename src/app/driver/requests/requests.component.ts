eb44a6af989c54a2183a81343579858e69fbbc6d
Jaay7 <jayakrishna.madem@gmail.com>
2022-06-14 17:11:41 +0530

    requests

--- src/app/driver/requests/requests.component.ts
+++ src/app/driver/requests/requests.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DriverService } from 'src/app/services/driver.service';
 
 @Component({
   selector: 'app-requests',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestsComponent implements OnInit {
 
-  constructor() { }
+  myRequests: any[] = [];
+
+  constructor(private driverService: DriverService) { }
 
   ngOnInit(): void {
+    this.driverService.getRequestedDealers(Number(localStorage.getItem('userId'))).subscribe({
+      next: (response: any) => {
+        this.myRequests = response;
+        console.log(response);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
   }
 
+  removeRequest(id: number, dealerId: number): void {
+    this.driverService.removeRequest(id, dealerId, Number(localStorage.getItem('userId'))).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
+  acceptRequest(id: number, dealerId: number): void {
+    this.driverService.acceptDealer(id, dealerId, Number(localStorage.getItem('userId'))).subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
 }
